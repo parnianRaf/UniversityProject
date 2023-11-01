70c65a76a0a435defc1e47f5c6a93d72b5957b38
parnianRaf <parnian.rafie58@gmail.com>
2023-11-01 18:24:36 +0330

    first empty Project

---
+++ UniversityProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
